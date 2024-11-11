fb1f76d11cf433a2c16e139ecd1947ac9b0d1b74
Shanie <shanieportal@gmail.com>
2024-11-11 00:51:49 -0500

    Fixed file organization for Flask, corrected some pathing

--- UIX/app.py
+++ UIX/app.py
@@ -6,5 +6,18 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/directory')
+def directory():
+    return render_template('directory.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
