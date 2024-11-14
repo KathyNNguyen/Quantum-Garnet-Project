ab76d8d93e65ddf96f3222812336b90764438246
KathyNNguyen <kathynthi@outlook.com>
2024-11-14 15:36:47 -0500

    added try block to confirm database has been populated

--- dbPopulate.py
+++ dbPopulate.py
@@ -3,11 +3,16 @@ import sqlite3
 conn = sqlite3.connect('slot_machines.db')
 cursor = conn.cursor()
 
-with open('db.sql', 'r') as f:
+with open('UIX/static/js/db.sql', 'r') as f:
     sql = f.read()
-    
-cursor.executescript(sql)
 
-conn.commit()
+try:
+    cursor.executescript(sql)
+    conn.commit()
+    print("Database populated successfully")
+except sqlite3.Error as e:
+    print(f"An error occurred: {e}")
+
+finally:
 cursor.close()
 conn.close()
